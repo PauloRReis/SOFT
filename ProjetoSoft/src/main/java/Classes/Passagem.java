6bf301d3b29807c6c256a7172d58fe952ce27ee7
Paulo Ricardo dos Reis <paulo230301@hotmail.com>
2023-07-02 14:33:44 -0300

    projeto software

---
+++ ProjetoSoft/src/main/java/Classes/Passagem.java
@@ -0,0 +1,27 @@
+package Classes;
+
+public class Passagem {
+    private int id;
+    private Float preco;
+
+    public Passagem(int id, Float preco) {
+        this.id = id;
+        this.preco = preco;
+    }
+    //getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Float preco) {
+        this.preco = preco;
+    }
+}
